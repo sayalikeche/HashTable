462007c94a374da0c36059efd3d668b894214443
sayalikeche <kechesayali11@gmail.com>
2021-11-24 06:45:42 +0530

    [sayali added Ability To Find Frequency Of Word]

--- HashTablee/HashTablee/Program.cs
+++ HashTablee/HashTablee/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using HashTable;
 
-namespace HashTablee
+namespace Datastructure
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to HashTable");
-            Console.ReadLine();
+            Console.WriteLine("HashTable");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value :" + hash5);
+
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value :" + hash2);
         }
     }
+
+   
 }
